78c54ad89f92d7d144a71bd4c554b0c7d1429adb
“nieguanfeng” <nieguanfeng@sina.com>
2025-02-23 20:22:46 +0800

    修改参数

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,29 +1,33 @@
+"use server";
 import { CardCompact } from "@/components/card-compact";
-// import { TicketUpdateForm } from "@/features/ticket/compoents/ticket-update-form";
 import { TicketUpsertForm } from "@/features/ticket/compoents/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 import { notFound } from "next/navigation";
-type TicketEditPageProps = {
-  params: {
-    ticketId: string;
-  }
-}
 
-const TicketEditPage = async ({params}:TicketEditPageProps) => {
-  const ticket = await getTicket(params.ticketId);
-  if(!ticket) {
+type TicketEditPageProps = {
+  params: Promise<{ticketId: string}>
+};
+const TicketEditPage:React.FC<TicketEditPageProps> = async ({ params }) => {
+  const { ticketId } = await params;
+  if (!ticketId) {
     notFound();
   }
+
+  const ticket = await getTicket(ticketId);
+
+  if (!ticket) {
+    notFound();
+  }
+
   return (
     <div className="flex-1 flex flex-col justify-center items-center">
       <CardCompact 
         title="Edit Ticket" 
         description="Edit a ticket" 
         className="w-full max-w-[420px]"
-        // content={<TicketUpdateForm ticket={ticket} />} 
         content={<TicketUpsertForm ticket={ticket} />}
       />
     </div>
   );
-} 
+}
 export default TicketEditPage;
