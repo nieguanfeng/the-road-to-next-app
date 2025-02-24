4642011c54ca86724b156e76379a5f770a386129
“nieguanfeng” <nieguanfeng@sina.com>
2025-02-24 09:12:31 +0800

    代码优化

--- src/components/theme/thteme-switcher.tsx
+++ src/components/theme/thteme-switcher.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 import { LucideMoon, LucideSun } from "lucide-react";
@@ -11,19 +10,19 @@ const ThemeSwitcher = () => {
       variant={"outline"}
       size={"icon"} 
       onClick={()=>setTheme(theme === 'light'?'dark':'light')}>
-        <LucideSun 
+        <LucideSun
           className="
-            h-4 w-4 rotate-0 scale-100 transition-all
+            h-4 w-4 rotate-0 scale-100 ml-6 transition-all
             dark:-rotate-90 dark:scale-0
           "
         />
-        <LucideMoon 
+        <LucideMoon
           className="
             absoulte h-4 w-4 rotate-90 scale-0 transition-transform
-            dark:rotate-0 dark:scale-100
+            dark:rotate-0 dark:scale-100 dark:-ml-10
           "
         />
-        <span className="sr-only">Toggle theme</span>
+        {/* <span className="sr-only">Toggle theme</span> */}
       </Button>
   );
 };
